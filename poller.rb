1e335ed037fff0eecfa1f7fb937885bb3f8eb863
Brian Borncamp <borncamp@debian>
2015-03-04 13:06:32 -0500

    new avatar

--- poller.rb
+++ poller.rb
@@ -1,11 +1,11 @@
 require 'open-uri'
 require 'nokogiri'
 
-url="http://www.suesnydeli.com/?_escaped_fragment_=daily-specials/ccf6"
+@url="http://www.suesnydeli.com/?_escaped_fragment_=daily-specials/ccf6"
 
-uri=open url
-doc = Nokogiri::HTML(uri)
-
-special=doc.xpath('//*[@id="hxxky9l6"]/p[13]/font').text
-
-puts special
+def poll
+  uri=open @url
+  doc = Nokogiri::HTML(uri)
+  
+  special=doc.xpath('//*[@id="hxxky9l6"]/p[13]/font').text
+end 
