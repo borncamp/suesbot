3b77e586ec70218f859207e39ecf61b958debc5a
Brian Borncamp <borncamp@debian>
2015-03-04 12:09:14 -0500

    first commit

---
+++ poller.rb
@@ -0,0 +1,11 @@
+require 'open-uri'
+require 'nokogiri'
+
+url="http://www.suesnydeli.com/?_escaped_fragment_=daily-specials/ccf6"
+
+uri=open url
+doc = Nokogiri::HTML(uri)
+
+special=doc.xpath('//*[@id="hxxky9l6"]/p[13]/font').text
+
+puts special
